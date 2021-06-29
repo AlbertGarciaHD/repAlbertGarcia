8b787e476508d9eb054a38d5176e32efadd29b0d
AlbertGarciaHD <albertgarcia990@gmail.com>
2021-06-29 16:14:02 -0400

    Tarea 2 js

--- Tarea 2 - JavaScript/app.js
+++ Tarea 2 - JavaScript/app.js
@@ -3,6 +3,7 @@ const boton = document.getElementById("boton")
 
 const accion = () => {
 
+    const colores = document.getElementById("paleta")
     const ancho = document.getElementById("ancho-cuadro")
     const altura = document.getElementById("altura-cuadro")
     const infor = document.getElementById("Informacion")
@@ -12,6 +13,9 @@ const accion = () => {
     contenedor.style.width = ancho.value;
     contenedor.style.height = altura.value;
     document.getElementById("contenedor").innerHTML=infor.value;
+    document.getElementById("contenedor").style.backgroundColor = colores.value;
+
+    whi
 }
 
 
